1185754adc8b6b2bb521b944a270acd101837a60
dee-raj <dhurbaraj343sky@gmail.com>
2026-04-02 16:46:32 +0530

    fix: release keystore added

--- android/app/src/main/java/com/deeraj/gamegroup/MainActivity.kt
+++ android/app/src/main/java/com/deeraj/gamegroup/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.deeraj.GameGroup
+package com.deeraj.gamegroup
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/deeraj/gamegroup/MainApplication.kt
+++ android/app/src/main/java/com/deeraj/gamegroup/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.deeraj.GameGroup
+package com.deeraj.gamegroup
 
 import android.app.Application
 import android.content.res.Configuration
